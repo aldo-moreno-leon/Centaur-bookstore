60126f92b8a4e58f835b16c6de88e57f851c41a8
aldo-moreno-leon <aldo.moreno.leon@protonmail.com>
2022-05-04 14:08:21 -0700

    .

---
+++ uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1651194236;
+// updated_on=1651194236;
+exit(0);
+?>
+1651194236_1881:"Debug: aldoadminnx, 177.228.75.2; Sucuri plugin has been uninstalled"
---
+++ uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1651194236;
+// updated_on=1651194236;
+exit(0);
+?>
